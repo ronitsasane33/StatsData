9a52af15bfb5fe2865bdde2c168a810190e027b0
Shubham Sasane <shubhamsasane10@gmail.com>
2021-05-11 18:42:22 -0400

    added housing

--- app.py
+++ app.py
@@ -5,10 +5,13 @@ import pickle
 app = Flask(__name__)
 emissionModel = pickle.load(open('emissionModel.pkl', 'rb'))
 irisModel = pickle.load(open('irisModel.pkl', 'rb'))
+housingModel = pickle.load(open('housingModel.pkl', 'rb'))
+
+# Page URLs
 
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('home.html')
 
 @app.route('/iris')
 def iris():
@@ -18,13 +21,11 @@ def iris():
 def o2emission():
     return render_template('o2emission.html')
 
-# CO2 Emission route
-@app.route('/predictEmission',methods=['POST'])
-def predictEmission():
-    init_features = [float(x) for x in request.form.values()]
-    final_features = [np.array(init_features)]
-    prediction = emissionModel.predict(final_features) 
-    return render_template('o2emission.html', prediction_text='Predicted Class: {}'.format(prediction)) 
+@app.route('/housing')
+def housing():
+    return render_template('housing.html')
+
+# URLS for processing Models
 
 # Iris route
 @app.route('/predictIris',methods=['POST'])
@@ -32,7 +33,25 @@ def predictIris():
     init_features = [float(x) for x in request.form.values()]
     final_features = [np.array(init_features)]
     prediction = irisModel.predict(final_features) 
-    return render_template('iris.html', prediction_text='Predicted Class: {}'.format(prediction)) 
+    return render_template('iris.html', prediction_text='Predicted Class: {}'.format(abs(prediction[0])))
+
+# CO2 Emission route
+@app.route('/predictEmission',methods=['POST'])
+def predictEmission():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = emissionModel.predict(final_features) 
+    return render_template('o2emission.html', prediction_text='Predicted Class: {}'.format(abs(prediction[0])))
+
+# Housing route
+@app.route('/predictHousing',methods=['POST'])
+def predictHousing():
+    init_features = [float(x) for x in request.form.values()]
+    final_features = [np.array(init_features)]
+    prediction = housingModel.predict(final_features) 
+    return render_template('housing.html', prediction_text='Predicted Class: {}'.format(abs(prediction[0]))) 
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ housing.py
@@ -0,0 +1,21 @@
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+
+data = pd.read_csv('BostonHousing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+X_train, X_test, y_train, y_test = train_test_split(features, prices, test_size=0.2, random_state=0)
+
+
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+pickle.dump(regressor, open('housingModel.pkl','wb'))
+
+
+
+
